4eea1d0a1e76c7c20ff15995a3e47df72ccf7113
tatcn8 <96024645+tatcn8@users.noreply.github.com>
2022-01-25 14:11:29 -0600

    added eslint for deployment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+/*eslint-disable no-unused-vars */
 
 ReactDOM.render(
   <React.StrictMode>
